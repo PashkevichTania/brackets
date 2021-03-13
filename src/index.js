c51f4d9d7e6e3bd55c63419d0ac13083221bc539
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-13 20:08:17 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const config_arr = bracketsConfig.map(item => item.join(''));
+    let string = str, tmp;
+
+    do {
+        tmp = string;
+        config_arr.forEach(item => string = string.replace(item, ''));
+    } while (string.length < tmp.length)
+
+    return !string.length;
 }
